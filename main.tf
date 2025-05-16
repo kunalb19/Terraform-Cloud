d3635b2e21d2e50bb8155358837320b9f22714ef
kunalb19 <76057071+kunalb19@users.noreply.github.com>
2025-05-16 11:01:54 +0530

    Added Ec2 resource

--- main.tf
+++ main.tf
@@ -9,3 +9,11 @@ resource "aws_key_pair" "keypair" {
   public_key = tls_private_key.keypair.public_key_openssh
   tags       = local.tags
 }
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS
+  instance_type = "t2.micro"
+  tags = {
+    Name = local.name
+  }
+}
