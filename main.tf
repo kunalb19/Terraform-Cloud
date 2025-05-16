b63347cc74106c54580fcfc16d5c7cb598723714
kunalb19 <76057071+kunalb19@users.noreply.github.com>
2025-05-16 11:11:28 +0530

    Added key

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "aws_key_pair" "keypair" {
 resource "aws_instance" "ubuntu" {
   ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.keypair.key_name
   tags = {
     Name = local.name
   }
